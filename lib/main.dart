dddc9d6c8493e63233ca6676a525f63ca696f4f0
Matteo <mattscimone@gmail.com>
2022-11-15 12:09:55 +0100

    Exo boule magique

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          body: ballMagic(),
+        ),
       ),
     );
+
+class ballMagic extends StatefulWidget {
+  @override
+  State<ballMagic> createState() => _ballMagicState();
+}
+
+class _ballMagicState extends State<ballMagic> {
+  int BallNumber = 1;
+  void changeBall() {
+    setState(() {
+      //rightDiceNumber = 6;
+      BallNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  changeBall();
+                },
+                child: Image.asset('images/ball$BallNumber.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}

4b16fb52ddb232e0940022ca65c07910123fd802
arunc <aruncaroo@gmail.Com>
2022-11-15 11:56:37 +0100

    travail Arun

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,59 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(BallPage());
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+              title: Text('Ask Me Anything'),
+              backgroundColor: Colors.indigo.shade900,),
+      body: Container(
+
+        child: Center(
+
+
+
+        child: Ball(),
+
       ),
+      ),
+      )
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(child: TextButton(
+            onPressed: () {
+              setState(() {
+                ballNumber = Random().nextInt(5) +1;
+              });
+            },
+          child: Image.asset('images/ball$ballNumber.png'),
+          ))
+        ],
+      )
+    );
+  }
+}
+
+
+
+
+

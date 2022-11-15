73c080fb20ddc3fc57687f70f5da27b9804b3247
Mehdi DIAH <mdiah@lerebours.org>
2022-11-15 11:26:28 +0100

    Application numéro 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Ask me everything'),
+            backgroundColor: Colors.indigo,
+          ),
+          backgroundColor: Colors.grey.shade900,
+          body: Ball()),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(4) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+      color: Colors.blue,
+      alignment: Alignment.center,
+    );
+  }
+}

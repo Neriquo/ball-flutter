a49f35ebf82a8661ad522b8d15ef460e50de9d3a
VINCENT GUILLAUME <vg.vince.guillaume@gmail.com>
2022-11-15 12:08:09 +0100

    Version fini

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,62 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Magic Ball'),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: BallPage(),
       ),
+    ),
+  );
+
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BallPageStateFul();
+  }
+}
+
+class BallPageStateFul extends StatefulWidget {
+  @override
+  State<BallPageStateFul> createState() => _BallPageStateFul();
+}
+
+class _BallPageStateFul extends State<BallPageStateFul> {
+
+  Random random = new Random();
+
+  int ball = 1;
+
+  void BallRandom() {
+    setState(() {
+      ball = random.nextInt(5)+1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+      Expanded(
+      child: TextButton(
+      onPressed: () {
+        BallRandom();
+      },
+      child: Image.asset('images/ball$ball.png')
+    )
+      )
+      ],
+      )
     );
+  }
+}
+

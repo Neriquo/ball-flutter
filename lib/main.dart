6bfdd33311c7f3e616301ab09166af516c6883de
Romain BRUNEAU <romaingm.bruneau@gmail.com>
2022-11-16 10:01:57 +0100

    Application fini

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,58 @@
+// importing flutter and dart packages
 import 'package:flutter/material.dart';
+import 'dart:math';
 
+// Creates a Material App
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+// Creates a Scaffold with
+// appbar using Stateless widget
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      appBar: AppBar(
+        backgroundColor: Colors.green[600],
+        title: Text('Magic-8-ball de Romain BRUNEAU'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+// Creates a Stateful widget
+class Ball extends StatefulWidget {
+  Ball({Key key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+
+  // Returns app with centered image TextButton
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            // Random.nextInt(n) returns random
+            // integer from 0 to n-1
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+
+        // Adding images
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
